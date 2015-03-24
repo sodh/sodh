321cccdd1e28424e02f8ec8dc035d6a193ede066
Ashish Acharya <ashish.adda@gmail.com>
2015-03-24 23:00:38 +0545

    Wrote some views code

--- website/pages/views.py
+++ website/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpRequest
 from django.template import RequestContext, loader
+from django.contrib import messages
 
 def home(request):
     '''
@@ -9,8 +10,19 @@ def home(request):
 
     template = loader.get_template('pages/index.html')
 
+    if request.method == 'POST':
+        command = request.POST.get('command')
+        selectedFile = request.POST.get('selectedFIle')
+
+        if command is not '':
+            messages.success(request, "Please wait while \"%s\" is executed on %s." % (command, selectedFile) )
+        else:
+            messages.error(request, "Please type in a valid command.")
+
     context = RequestContext(request, {
         'title': "Sodh : Generic Research Assistant",
         'menuindex': 1,
+        'messages': messages,
     })
+
     return HttpResponse(template.render(context))
--- website/sodh/settings.py
+++ website/sodh/settings.py
@@ -90,3 +90,8 @@ TEMPLATE_DIRS=(
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,"static"),
     )
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
