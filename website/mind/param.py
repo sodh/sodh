9e3a216d5c925a5138aa8af485f012bb5315e753
Bidhan <bbidhan666@gmail.com>
2015-06-16 23:15:25 +0545

    added NumArray param for comma separated integers

--- website/mind/param.py
+++ website/mind/param.py
@@ -14,6 +14,17 @@ class Numeric(Param):
     def __str__(self):
         return str(self.value)
 
+class NumArray(Param):
+    def __init__(self,_keywords=None, _value = 1):
+        self.keywords = [re.compile(kw.replace("$","(?P<value>([1-9][0-9]*,)*[1-9][0-9]*)")) for kw in _keywords]
+        self.value = _value
+
+    def get(self):
+        return [int(x) for x in str(self.value).split(',')]
+
+    def __str__(self):
+        return str(self.value)
+
 class Alias(Param):
     def __init__(self,_keywords=None):
         self.keywords = [re.compile(kw) for kw in _keywords]
